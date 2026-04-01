472bb7248c1c34e940ab8f35827597efe123ebb1
Evelyn Chiaperini <evelynchiaperini@gmail.com>
2026-04-01 09:33:54 -0300

    copyright

--- estimativa-java-ocp/src/main/java/br/com/valueprojects/service/CalculadoraProdutividade.java
+++ estimativa-java-ocp/src/main/java/br/com/valueprojects/service/CalculadoraProdutividade.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2026 Evelyn Chiaperini
+ *
+ * Este código foi desenvolvido para fins acadêmicos como parte de atividades
+ * educacionais.
+ *
+ * Não é permitida a cópia, redistribuição ou utilização deste código,
+ * total ou parcial, para entrega em trabalhos acadêmicos por terceiros,
+ * caracterizando plágio.
+ *
+ * É permitido o uso como referência para estudo, desde que seja dado o
+ * devido crédito ao autor original.
+ *
+ * Este software é fornecido "como está", sem garantias de qualquer tipo,
+ * expressas ou implícitas. O autor não se responsabiliza por quaisquer
+ * danos decorrentes do uso deste código.
+ */
+
 package br.com.valueprojects.service;
 
 public interface CalculadoraProdutividade {
